d968faf5d5992deca11d7a385b0e9d98e19c431a
vinnidias <vinicius.3991@gmail.com>
2022-08-01 12:46:12 -0300

    Home screen initial components

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
 import { THEME } from './src/styles/theme';
 import { Loading } from './src/components/Loading';
 
@@ -16,7 +17,7 @@ export default function App() {
         translucent
       />
       {
-        fontsLoaded ? <SignIn /> : <Loading/>
+        fontsLoaded ? <Home /> : <Loading/>
       }
     </NativeBaseProvider>
 
